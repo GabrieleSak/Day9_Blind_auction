60912015acedfb132a684a81713372ccfc2bbb71
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-09-14 13:41:16 +0300

    Add inputs from user

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ from art import logo
 
 print(logo)
 
+new_bidder = True
+while new_bidder:
+
+    name = input("What is your name?: ")
+    bid = input("What is your bid?: $")
+
+    more_bidders = input("Are there any other bidders? Type 'yes' or 'no'.")
+    if more_bidders == 'no':
+        new_bidder = False
+    clear()
