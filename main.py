ef7a147692fa9396b0416e1caa2e4861a018a381
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-09-14 13:51:00 +0300

    Add bids to dictionary and find which is the biggest

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ from art import logo
 print(logo)
 
 new_bidder = True
+auction = {}
 while new_bidder:
 
     name = input("What is your name?: ")
-    bid = input("What is your bid?: $")
+    bid = int(input("What is your bid?: $"))
 
-    more_bidders = input("Are there any other bidders? Type 'yes' or 'no'.")
-    if more_bidders == 'no':
+    auction[name] = bid
+
+    more_bidders = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+    if more_bidders == 'yes':
+        clear()
+        print("clear()")
+    else:
         new_bidder = False
-    clear()
+        max_bid = 0
+        for bidder in auction:
+            if auction[bidder] > max_bid:
+                max_bid = auction[bidder]
+        print(f"The winner is {bidder} with a bid of ${max_bid}!")
+
+
